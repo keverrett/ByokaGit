286db942c0c20bb68f2478a18db54ada10b80920
kevin everrett <everrett@kevins-imac.home>
2021-08-29 13:30:20 -0400

    Setting up ProductSceen and HomeScreen to receive data from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 
 const ProductScreen = ({ match }) => {
     
-    const product = products.find(p => p.id === match.params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            
+            setProduct(data)
+        }
+        fetchProducts()
+    }, [] )
     
     return (
         <>
