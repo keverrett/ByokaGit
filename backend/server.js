b924918a81be7dd3829cb77fcd655fdc751c6d8e
kevin everrett <everrett@kevins-imac.home>
2021-08-29 21:23:41 -0400

    Setting up MongoDB and PostMan

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+
 
 dotenv.config()
+
+connectDB()
 
 
 const app = express()
@@ -11,16 +18,13 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 } )
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-} )
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p.id === req.params.id)
-    res.json(product)
-} )
+app.use(notFound)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
